e97e3367585486671a2f30f05ce3e459c9d86f83
Russell Heilling <russell@heilling.net>
2013-07-11 17:41:34 +0100

    Handle floating issue numbers better (.5 and .1 issues)

--- ooo.py
+++ ooo.py
@@ -5,7 +5,7 @@ import sys
 import re
 from collections import defaultdict
 
-COMIC_RE = re.compile(r'^\d+ +([^#]+)#(\d+)')
+COMIC_RE = re.compile(r'^\d+ +([^#]+)#([\d.]+)')
 
 def lines(todofile):
   with open(todofile) as todolines:
@@ -13,19 +13,22 @@ def lines(todofile):
       title_match = COMIC_RE.match(line)
       if title_match:
         # (title, issue)
-        yield line.strip(), title_match.group(1), int(title_match.group(2))
+        yield line.strip(), title_match.group(1), title_match.group(2)
 
 def issues(todofile):
   seen = defaultdict(int)
   for line, title, issue in lines(todofile):
-    if issue and seen[title] and abs(issue - seen[title]) > 1:
-      yield line, seen[title]
-    seen[title] = issue
+    if issue and issue != '0':
+      if seen[title]:
+        delta = abs(float(issue) - float(seen[title]))
+        if delta == 0 or delta > 1:
+          yield line, seen[title]
+      seen[title] = issue
 
 def main(files):
   for todofile in files:
     for issue, lastissue in issues(todofile):
-      print "%s (last seen %d)" % (issue, lastissue)
+      print "%s (last seen %s)" % (issue, lastissue)
 
 if __name__ == '__main__':
   main(sys.argv[1:])
