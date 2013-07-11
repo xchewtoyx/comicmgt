1dd8e21ac642015cb8c94ae8eddcaeaf619e5692
Russell Heilling <russell@heilling.net>
2013-07-11 17:41:34 +0100

    Handle floating issue numbers better (.5 and .1 issues)

--- ooo.py
+++ ooo.py
@@ -4,28 +4,11 @@ import os
 import sys
 import re
 from collections import defaultdict
-import args
-ARGS=None
-args.add_argument('--noreboots', '-r', action='store_true',
-                  help='ignore series reboots')
-args.add_argument('--nodups', '-d', action='store_true',
-                  help='ignore duplicates')
-args.add_argument('--maxdelta', '-m', type=int, default=50,
-                  help='Assume larger jumps are intentional')
-args.add_argument('files', nargs='*', default=[sys.stdin], 
-                  help='Files to merge')
 
-
-COMIC_RE = re.compile(r'^\d+ +([^#]+)#([^:\s]+)')
-
-def inputfile(todofile):
-  if hasattr(todofile, 'readline'):
-    return todofile
-  else:
-    return open(todofile)
+COMIC_RE = re.compile(r'^\d+ +([^#]+)#([\d.]+)')
 
 def lines(todofile):
-  with inputfile(todofile) as todolines:
+  with open(todofile) as todolines:
     for line in todolines:
       title_match = COMIC_RE.match(line)
       if title_match:
@@ -35,20 +18,17 @@ def lines(todofile):
 def issues(todofile):
   seen = defaultdict(int)
   for line, title, issue in lines(todofile):
-    if issue and issue.isdigit() and issue != '0':
+    if issue and issue != '0':
       if seen[title]:
         delta = abs(float(issue) - float(seen[title]))
-        if ((delta == 0 and not ARGS.nodups) or 
-            (delta > 1 and delta < ARGS.maxdelta and not (
-              int(issue) == 1 and ARGS.noreboots))):
+        if delta == 0 or delta > 1:
           yield line, seen[title]
       seen[title] = issue
 
-def main():
-  for todofile in ARGS.files:
+def main(files):
+  for todofile in files:
     for issue, lastissue in issues(todofile):
       print "%s (last seen %s)" % (issue, lastissue)
 
 if __name__ == '__main__':
-  ARGS = args.parse_args()
-  main()
+  main(sys.argv[1:])
