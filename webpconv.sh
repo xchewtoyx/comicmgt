8d24f2da006c900fd62e7f10114378e77a224588
Russell Heilling <russell@heilling.net>
2013-07-28 12:36:59 +0100

    Convert cbz file with webp files to cbr with jpeg images

---
+++ webpconv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+TMPDIR=$(mktemp -d)
+
+file=$1
+if [ -z "$file" ]; then
+  echo Give a filename >/dev/stderr
+  exit 1
+fi
+
+if [ ! -f "$file" ]; then
+  echo File not found $file > /dev/stderr
+  exit 1
+fi
+
+if [ -f "${file/%cbz/cbr}" ]; then
+  echo Output file already exists. Not clobbering. > /dev/stderr
+  exit 1
+fi
+
+/usr/bin/unzip -q "$file" -d $TMPDIR
+
+for page in $TMPDIR/*.webp; do
+  pngfile=$(mktemp --tmpdir="$TMPDIR")
+  dwebp "$page" -o "$pngfile" > /dev/null
+  /usr/bin/convert "$pngfile" "${page/%webp/jpg}"
+done
+
+/usr/bin/rar a -ep -inul "${file/%cbz/cbr}" $TMPDIR/*.{jpg,png}
+
+rm -rf $TMPDIR
