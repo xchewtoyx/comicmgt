8a228322f3a65f57f75faa8b9b0a11555bea4e26
Russell Heilling <russell@heilling.net>
2013-07-30 12:20:31 +0100

    added reading streams

--- sort-readlist.py
+++ sort-readlist.py
@@ -6,52 +6,141 @@ import subprocess
 import sys
 import os
 
+from collections import defaultdict
+
 import calibre_config
 from calibre.library.database2 import LibraryDatabase2
 from calibre.utils.config import prefs
 
 import args
 
-TODO_DIR = os.path.join(os.environ['HOME'], 'Dropbox', 'todo')
 ISSUE_PATTERN = re.compile('(\d+) (.*)$')
 
+args.add_argument('--archive', '-a', help='Archive file before sorting',
+                  action='store_true')
 args.add_argument('--todobin', '-t', help='path to todo.txt script',
                   type=str, default='todo.sh')
-args.add_argument('--todofile', '-f', help='path to todo.txt file',
-                  type=str, default=os.path.join(TODO_DIR, 'todo.txt'))
+args.add_argument('--infile', '-i', help='path to input file',
+                  default=sys.stdin)
+args.add_argument('--outfile', '-o', help='path to output file',
+                  default=sys.stdout)
 args.add_argument('--verbose', '-v', help='Enable verbose logging',
                   action='store_true')
 
 ARGS={}
 
-def get_issues():
-  for line in open(ARGS.todofile, 'r'):
+class StreamClassifier(object):
+  def __init__(self):
+    self.streams = ['catchup', 'marvel', 'dc', 'valiant']
+    self.catchup_volumes = ['18436', '18519', '18520', '18521']
+
+  def stream_catchup(self, mi):
+    volume = mi.identifiers.get('comicvine-volume')
+    return volume and volume in self.catchup_volumes
+      
+  def stream_marvel(self, mi):
+    return mi.publisher in ['Marvel', 'Max']
+
+  def stream_dc(self, mi):
+    return mi.publisher in ['DC', 'DC Comics']
+
+  def stream_valiant(self, mi):
+    return mi.publisher in ['Valiant']
+
+  def classify(self, mi):
+    for stream in self.streams:
+      classifier = getattr(self, 'stream_'+stream)
+      if classifier and classifier(mi):
+        return stream
+    return None
+
+def get_issues(infile):
+  'Find issues listed in "id title" format.'
+  for line in infile:
     issueid = ISSUE_PATTERN.match(line)
     if issueid:
-      yield int(issueid.group(1)), issueid.group(2)
+      issue_id, issue_name = issueid.groups()
+      logging.debug('Found issue "%s"(%d) [%s]', issue_id, issue_name)
+      yield int(issue_id), issue_name
+    else:
+      logging.warn('Unable to parse line: %s', line)
 
-def get_issue_details():
+def get_issue_details(infile):
+  'Look up issue details in Calibre library.'
   db = LibraryDatabase2(prefs['library_path'])
-  for issue, title in get_issues():
+  classifier = StreamClassifier()
+  for issue, title in get_issues(infile):
     mi = db.get_metadata(issue, index_is_id=True)
-    yield mi.pubdate, title, issue
+    if mi:
+      stream = classifier.classify(mi)
+      logging.debug('Found issue %s(%d) [%s] [%s]', 
+                    title, issue, stream, mi.pubdate)
+      # Return tuple with first entry being a tuple of the sort keys
+      yield (mi.pubdate, mi.title_sort), issue, title, stream
+    else:
+      logging.warn('Unable to find issue in database: %s(%d)', title, issue)
+
+def get_streams(infile):
+  streams = defaultdict(list)
+  for sortkey, issue, title, stream in get_issue_details(infile):
+    streams[stream].append((sortkey, issue, title))
+  for stream in streams:
+    streams[stream].sort()
+  return streams
+
+def merged_streams(infile):
+  streams = get_streams(infile)
+  items = sum([len(streams[stream]) for stream in streams])
+  weight = {}
+
+  for stream in streams:
+    weight[stream] = len(streams[stream]) / (1.0 * items)
+
+  for i in range(items):
+    collected = defaultdict(float)
+    for stream in streams:
+      if streams[stream]:
+        collected[stream] += weight[stream]
+        if collected >= 1.0:
+          yield streams[stream].pop(0)
+          collected[stream] -= 1.0
+
+  for stream in streams:
+    if len(streams[stream]):
+      for item in streams[stream]:
+        yield item
 
 def main():
   logger = logging.getLogger()
   if ARGS.verbose:
     logger.setLevel(logging.DEBUG)
+
   # archive todo list
-  try:
-    output = subprocess.check_output([ARGS.todobin, 'archive'])
-  except CalledProcessError as e:
-    logging.error('Unable to archive old items: %s', e.output)
-  issues = sorted(get_issue_details())
+  if ARGS.archive:
+    try:
+      output = subprocess.check_output([ARGS.todobin, 'archive'])
+    except CalledProcessError as e:
+      logging.error('Unable to archive old items: %s', e.output)
+
+  # Open input and sort by pubdate then name
+  infile = ARGS.infile
+  if isinstance(infile, basestring):
+    infile = open(infile, 'r')
+  issues = list(merged_streams(infile))
+  if infile is not sys.stdin:
+    infile.close()
     
   # Write out sorted list
-  for pubdate, title, idx in issues:
-    print "%d %s" % (idx, title)
+  outfile = ARGS.outfile
+  if isinstance(outfile, basestring):
+    outfile = open(outfile, 'w')
+  for __, issue, title in issues:
+    outfile.write('%d %s\n' % (issue, title))
+  if outfile is sys.stdout:
+    outfile.flush()
+  else:
+    outfile.close()
 
 if __name__ == '__main__':
   ARGS = args.parse_args()
   main()
-
