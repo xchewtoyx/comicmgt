625f867a8491608b975dd41919376f72ffed0c55
Russell Heilling <russell@heilling.net>
2013-08-01 17:55:46 +0100

    Add warning when stream gets low weight

--- sort-readlist.py
+++ sort-readlist.py
@@ -53,7 +53,7 @@ class StreamClassifier(object):
     volume = mi.identifiers.get('comicvine-volume')
     if volume and volume in self.catchup_streams:
       self.catchup_seen.add(volume)
-      return self.catchup_streams[]
+      return self.catchup_streams[volume]
       
   def stream_marvel(self, mi):
     if mi.publisher in ['Marvel', 'Max']:
@@ -130,6 +130,12 @@ def merged_streams(infile):
 
   for stream in streams:
     weight[stream] = len(streams[stream]) / (1.0 * max_stream)
+    # A weight of less than 1/10 is probably a good indication that
+    # there are not enough issues for the stream to be effective
+    if weight[stream] < 0.1:
+      logging.warn('Stream %s has weight of %0.4f which will result in  '
+                   'significant gaps between issues.  Consider removing '
+                   'this stream or merging it with another.')
     logging.debug('Stream length[%s]: %d', stream, len(streams[stream]))
     logging.debug('Stream weight[%s]: %0.4f', stream, weight[stream])
 
