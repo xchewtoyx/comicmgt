145df958a488767f174e526d08307ed44da4d495
Russell Heilling <russell@heilling.net>
2013-09-03 07:50:13 +0100

    Add updating volume start dates to pullck, and make both this new check and the existing issue updates flag controlled.

--- pullck.py
+++ pullck.py
@@ -6,6 +6,9 @@ from datetime import datetime, timedelta
 import logging
 import os
 
+import pycomicvine
+
+import api_key
 import args
 from pulldb import PullList
 import logs
@@ -13,26 +16,13 @@ from calibredb import CalibreDB
 
 args.add_argument('--pulldb', '-d', help='location of pull database',
                   default=os.path.join(os.environ['HOME'], '.pull.db'))
-args.add_argument('--list', '-l', help='List volumes on pull-list.',
-                  default=True, action='store_true')
-args.add_argument('--nolist', help='Do not list volumes on pull-list.',
-                  dest='list', action='store_false')
-args.add_argument('--expire', '-x', action='store_true',
-                  help='Check for volumes with no issues for defined period.')
-args.add_argument('--expire_limit', '-e', help='Expiry period in days',
-                  default=90, type=int)
-args.add_argument('--check', '-c', help='Check for missing issues', 
-                  action='store_true')
-args.add_argument('--add', '-a', help='Add a volume to the pull list',
-                  action='append')
-args.add_argument('--remove', '-r', help='Remove a volume from the pull list',
-                  action='append')
+args.add_argument('--fixissues', action='store_true',
+                  help='Fill in comicvineid and volumeid for all issues')
+args.add_argument('--fixdates', action='store_true',
+                  help='Set volume start dates for all volumes where not set')
 ARGS = args.ARGS
 
-def main():
-  'Check for new issues to pull.'
-  pull_list = PullList(ARGS.pulldb)
-  calibredb = CalibreDB()
+def fix_issues(pull_list, calibredb):
   for volume in list(pull_list.volumes()):
     for issue in list(pull_list.seen_issues(volume)):
       issue = int(issue)
@@ -45,6 +35,27 @@ def main():
     pull_list.add_issue(
       issue, volumeid=int(metadata.identifiers.get('comicvine-volume')),
       cvid=int(metadata.identifiers.get('comicvine')))
+
+def fix_start_dates(pull_list):
+  for volume in list(pull_list.volumes()):
+    volume_start = pull_list.start_date(volume)
+    if volume_start and volume_start.date > datetime.min.date:
+      continue
+    volume_detail = pycomicvine.Volume(volume, field_list=[
+        'id', 'name', 'start_year'])
+    logging.info('Setting start year for volume: %s (%d) [%s]',
+                 volume_detail.name, volume, volume_detail.start_year)
+    start_date = datetime(volume_detail.start_year, 1, 1)
+    pull_list.start_date(volume, start_date=start_date)
+
+def main():
+  'Check for new issues to pull.'
+  pull_list = PullList(ARGS.pulldb)
+  calibredb = CalibreDB()
+  if ARGS.fixissues:
+    fix_issues(pull_list, calibredb)
+  if ARGS.fixdates:
+    fix_start_dates(pull_list)
 
 if __name__ == '__main__':
   args.parse_args()
