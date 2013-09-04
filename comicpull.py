d8e83571bbe270682f713af8b10282d4f7446dbc
Russell Heilling <russell@heilling.net>
2013-09-04 07:16:02 +0100

    Remove add action as that is now covered by pull-list.py
    Remove option to skip the pull, as without the add action that is all
    this script now does.
    
    Allow script to be run sharded.  Experimentation with threading didn't
    really show much of a performance improvement.  New behaviour allows you
    to run a job for just a subset of volumes.  You can then run a number of
    these jobs independently.
    
    I am running this using a crontab to run it once an hour, with 24
    workers and processing shard $(date +%H) (i.e. during a 24 hour period
    all volumes will be updated).  This means that my pull database is
    locked for approx 1 minute per hour rather than 16 minutes all in one
    go.

--- comicpull.py
+++ comicpull.py
@@ -16,44 +16,55 @@ from toread import ReadingList
 
 ARGS = args.ARGS
 
-args.add_argument('--add_volume', '-a', action='append',
-                  help='comicvine volume to add to pull-list.')
-args.add_argument('--nopull', '-n', action='store_true',
-                  help='Don\'t check for new issues')
 args.add_argument('--todo_file', help='Location of todo.txt file',
                   default=os.path.join(os.environ['HOME'],
                                        'Dropbox/todo/todo.txt'))
 args.add_argument('--pulldb', '-p', help='Location of pull database',
                   default=os.path.join(os.environ['HOME'], '.pull.db'))
+args.add_argument('--workers', help='Number of worker threads.',
+                  default=1, type=int)
+args.add_argument('--shard', help='The task number to run',
+                  default=0, type=int)
+
+def check_new(volume, pull_list=None, calibredb=None):
+  logging.debug('Checking volume %d for new issues', volume)
+  seen_issues = pull_list.seen_issues(volume)
+  new_issues = set()
+  logging.debug('Checking for issues in Calibre Database for volume %d',
+                volume)
+  issues = calibredb.search(
+    query='identifiers:comicvine-volume:%d' % volume, return_matches=True)
+  for issueid in issues:
+    if issueid in seen_issues:
+      logging.debug('Issue %d already seen in volume %d', 
+                    issueid, volume)
+      continue
+    # Sometimes issues will be retagged after being pulled.  Double
+    # check that the issue isn't in the database associated with a
+    # different volume before pulling.
+    if pull_list.seen_issue(issueid):
+      logging.warn('Issue %d seen but not associated with volume %d',
+                   issueid, volume)
+      continue
+
+    logging.info('Found unseen issue %d', issueid)
+    issue = calibredb.issue(issueid)
+    cvid = issue.identifiers.get('comicvine')
+    new_issues.add((issueid, issue.title, volume, cvid))
+  return new_issues
+
+def sharded_to_us(volume):
+  return volume % ARGS.workers == ARGS.shard
 
 def pull_issues(pull_list):
   'Check for unseen issues in database and add them to toread list.'
   calibredb = CalibreDB()
   # Check database for new issues for pull volumes
-  new_issues = []
+  new_issues = set()
   for volume in pull_list.volumes():
-    logging.info('Checking volume %d for new issues', volume)
-    seen_issues = pull_list.seen_issues(volume)
-    calibredb.search(query='identifiers:comicvine-volume:%d' % volume)
-    for issue in calibredb.data:
-      issueid = issue[calibredb.FIELD_MAP['id']]
-      identifiers = {}
-      for ident in issue[calibredb.FIELD_MAP['identifiers']].split(','):
-        identifiers.update((ident.split(':'),))
-      cvid = identifiers.get('comicvine')
-      
-      if issueid in seen_issues:
-        logging.debug('Issue %d already seen in volume %d', issueid, volume)
-        continue
-      # Sometimes issues will be retagged after being pulled.  Double
-      # check that the issue isn't in the database associated with a
-      # different volume before pulling.
-      if pull_list.seen_issue(issue['id']):
-        logging.warn('Issue %d seen but not associated with volume %d',
-                      issueid, volume)
-        continue
-      logging.info('Found unseen issue %d', issueid)
-      new_issues.append((issueid, issue['title'], volume, cvid))
+    if sharded_to_us(volume):
+      new_issues.update(
+        check_new(volume, pull_list=pull_list, calibredb=calibredb))
   # Update toread list
   if new_issues:
     toread = ReadingList(ARGS.todo_file)
@@ -64,13 +75,7 @@ def pull_issues(pull_list):
 def main():
   'Check for new issues to pull.'
   pull_list = PullList(ARGS.pulldb)
-  # Add new volumes
-  if ARGS.add_volume:
-    for volume in [int(vol) for vol in ARGS.add_volume]:
-      if not pull_list.pull_volume(volume):
-        pull_list.add_volume(int(volume))
-  if not ARGS.nopull:
-    pull_issues(pull_list)
+  pull_issues(pull_list)
 
 if __name__ == '__main__':
   args.parse_args()
