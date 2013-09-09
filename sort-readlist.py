7f890914dbcc12f4784d2ba723be847bcf9b8dce
Russell Heilling <russell@heilling.net>
2013-09-09 10:44:38 +0100

    Remove the no longer used archive function.

--- sort-readlist.py
+++ sort-readlist.py
@@ -11,25 +11,11 @@ from streams import StreamClassifier
 import calibredb
 import logs
 
-args.add_argument('--archive', '-a', help='Archive file before sorting',
-                  action='store_true')
-args.add_argument('--todobin', '-t', help='path to todo.txt script',
-                  type=str, default='todo.sh')
 args.add_argument('--infile', '-i', help='path to input file',
                   default=sys.stdin)
 args.add_argument('--outfile', '-o', help='path to output file',
                   default=sys.stdout)
 ARGS = args.ARGS
-
-def archive():
-  'Archive todo list.'
-  if ARGS.archive:
-    try:
-      output = subprocess.check_output([ARGS.todobin, 'archive'])
-    except subprocess.CalledProcessError as error:
-      logging.error('Unable to archive old items: %s', error.output)
-    logging.info('Archive successful: %s', output)
-
 
 def main():
   'Setup environment and run classification.'
